853b04d0a34b6495f7daae51e5a99d4519b6273c
Minh Le <duyminh.le310@gmail.com>
2022-08-21 20:58:49 -0400

    Add MainBody content

--- Components/MainBody/index.js
+++ Components/MainBody/index.js
@@ -1,6 +1,55 @@
-export default function Services () {
+import Image from 'next/image';
+
+import galleryPic1Img from '../../imgs/gallery-pic-1.jpg';
+import galleryPic2Img from '../../imgs/gallery-pic-2.jpg';
+import galleryPic3Img from '../../imgs/gallery-pic-3.jpg';
+
+import cupAwardImg from '../../imgs/cup-award.png';
+
+export default function MainBody () {
 
     return (
-        <h3>MainBody</h3>
+        <>
+            <BestPartner />
+            <Gallery />
+        </>
     )
 }
+
+const BestPartner = () => (
+    <div>
+        <div>
+            <h3>Best Partner</h3>
+            <Symbol />
+        </div>
+        <Image 
+            src={cupAwardImg}
+            alt="A cup award"
+        />
+        <p>We earn you the most revenue of operating your rental property.</p>
+    </div>
+);
+
+const Symbol = () => (
+    <div></div>
+)
+
+const Gallery = () => (
+    <div>
+        <div>
+            <Image 
+                src={galleryPic1Img}
+                alt="A room"
+            />
+            <Image 
+                src={galleryPic2Img}
+                alt="A room"
+            />
+            <Image 
+                src={galleryPic3Img}
+                alt="A room"
+            />
+        </div>
+        <div>Pagination</div>
+    </div>
+);
