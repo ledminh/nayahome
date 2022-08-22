839b929ba4548beecc131b2fabfbe9eb0eefbd00
Minh Le <duyminh.le310@gmail.com>
2022-08-21 20:47:41 -0400

    Add content to NavBar and Intro

--- Components/Intro/index.js
+++ Components/Intro/index.js
@@ -1,6 +1,36 @@
+import Image from 'next/image';
+import manTestimonialImg from '../../imgs/man-testimonial.jpg';
 export default function Intro () {
 
     return (
-        <h3>Intro</h3>
+        <>
+            <Slogan/>
+            <Testimony/>
+        </>
     )
 }
+
+
+const Slogan = () => (
+    <div>
+        <h1>Boost</h1>
+        <h1>your profits</h1>
+        <h3>from short-term and vacation rentals</h3>
+    </div>
+);
+
+const Testimony = () => (
+    <figure>
+        <Image 
+            src={manTestimonialImg}
+            alt="A smiling man"
+        />
+        <h4>This inn is so much more comfortable and lovely</h4>
+        <figcaption>
+            <cite>
+                <span>James B</span>
+                <span>GM-Reddoorz</span>
+            </cite>
+        </figcaption>
+    </figure>
+);
--- Components/NavBar/index.js
+++ Components/NavBar/index.js
@@ -1,6 +1,17 @@
+import Link from 'next/link';
+
 export default function NavBar () {
 
     return (
-        <h3>NavBar</h3>
+        <nav>
+            <h3>NayaHome</h3>
+            <ul>
+                <li><Link href="/property">PROPERTY</Link></li>
+                <li><Link href="/services">SERVICES</Link></li>
+                <li><Link href="/testimonial">TESTIMONIAL</Link></li>
+                <li><Link href="/about">ABOUT</Link></li>
+            </ul>
+            <button>LET{"'S"} TALK</button>
+        </nav>
     )
 }
