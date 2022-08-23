9cfb4d505fb15b4a398fa069d45d475239a48d8b
Minh Le <duyminh.le310@gmail.com>
2022-08-22 20:55:09 -0400

    MainBody

--- Components/MainBody/index.js
+++ Components/MainBody/index.js
@@ -1,8 +1,15 @@
+import styles from './MainBody.module.scss';
+import typographyStyles from '../../styles/Typography.module.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
 import Image from 'next/image';
 
 import galleryPic1Img from '../../imgs/gallery-pic-1.jpg';
 import galleryPic2Img from '../../imgs/gallery-pic-2.jpg';
 import galleryPic3Img from '../../imgs/gallery-pic-3.jpg';
+
+
+import { Carousel } from 'react-responsive-carousel';
 
 import cupAwardImg from '../../imgs/cup-award.png';
 
@@ -17,26 +24,34 @@ export default function MainBody () {
 }
 
 const BestPartner = () => (
-    <div>
-        <div>
-            <h3>Best Partner</h3>
+    <div className={styles.BestPartner}>
+        <div className={styles.title}>
+            <h4 className={typographyStyles.bold}>Best Partner</h4>
             <Symbol />
         </div>
-        <Image 
-            src={cupAwardImg}
-            alt="A cup award"
-        />
+        <div className={styles.Image}>
+            <Image 
+                src={cupAwardImg}
+                alt="A cup award"
+            />
+        </div>
         <p>We earn you the most revenue of operating your rental property.</p>
     </div>
 );
 
 const Symbol = () => (
-    <div></div>
+    <div className={styles.Symbol}>
+        <div /> <div />
+        <div /> <div />
+    </div>
 )
 
 const Gallery = () => (
-    <div>
-        <div>
+    <div className={styles.Gallery}>
+        <Carousel 
+            showArrows={true}
+            showThumbs={false}
+            >
             <Image 
                 src={galleryPic1Img}
                 alt="A room"
@@ -49,7 +64,6 @@ const Gallery = () => (
                 src={galleryPic3Img}
                 alt="A room"
             />
-        </div>
-        <div>Pagination</div>
+        </Carousel>
     </div>
 );
