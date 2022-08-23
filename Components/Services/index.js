650e1c494ac1deb16103c7c84b1677c6b5ae7e11
Minh Le <duyminh.le310@gmail.com>
2022-08-22 21:44:09 -0400

    Second round

--- Components/Services/index.js
+++ Components/Services/index.js
@@ -1,3 +1,5 @@
+import styles from './Services.module.scss';
+
 import Image from 'next/image';
 
 import airbnbIcon from '../../imgs/airbnb-icon.png';
@@ -6,33 +8,37 @@ import framerIcon from '../../imgs/framer.png';
 export default function Services () {
 
     return (
-        <ul>
-            <li>
+        <ul className={styles.Services}>
+            <li className={styles.li}>
                 <a>
-                    <Image 
-                        src={airbnbIcon}
-                        alt="Airbnb Icon"
-                    />
+                    <div className={styles.Image}>
+                        <Image 
+                            src={airbnbIcon}
+                            alt="Airbnb Icon"
+                        />
+                    </div>
                     <span>airbnb</span>
                 </a>
             </li>
-            <li>
+            <li className={styles.li}>
                 <a>
-                    <Image 
-                        src={framerIcon}
-                        alt="Framer Icon"
-                    />
+                    <div className={styles.Image}>
+                        <Image 
+                            src={framerIcon}
+                            alt="Framer Icon"
+                        />
+                    </div>
                     <span>Framer</span>
                 </a>
             </li>
-            <li><a><span>Booking.com</span></a></li>
-            <li>
+            <li className={styles.li}><a><span className={styles.Booking}>Booking.com</span></a></li>
+            <li className={styles.li}>
                 <a>
                     <span>afterpay</span>
                     <i className="fa fa-umbrella-beach"></i>
                 </a>
             </li>
-            <li>
+            <li className={styles.li}>
                 <a>
                     <i className="fa fa-solid fa-hand-spock"></i>
                     <span>treehouse</span>
