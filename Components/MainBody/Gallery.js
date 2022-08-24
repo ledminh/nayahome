5db5baca2ca86ef3e87cdc8741bada8b591fa51d
Minh Le <duyminh.le310@gmail.com>
2022-08-24 01:28:32 -0400

    Done Gallery

--- Components/MainBody/Gallery.js
+++ Components/MainBody/Gallery.js
@@ -8,29 +8,42 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 import styles from './Gallery.module.scss';
+import { useState } from 'react';
+
+
 
 export default function Gallery () {
-
+    const [currentImage, setCurrentImage] = useState(0);
 
     return (
         <div className={styles.Gallery}>
             <div className={styles.content_lg}>
                 <div className={styles.Image + ' ' + styles.Image_1}
-                    
+                    onClick={() => setCurrentImage(1)}
+                    onMouseLeave={() => setCurrentImage(0)}
+                    data-expanded={currentImage === 1}
                 >
                     <Image 
                         src={galleryPic1Img}
                         alt="A room"
                     />
                 </div>
-                <div className={styles.Image + ' ' + styles.Image_2}>
+                <div className={styles.Image + ' ' + styles.Image_2}
+                    onClick={() => setCurrentImage(2)}
+                    onMouseLeave={() => setCurrentImage(0)}
+                    data-expanded={currentImage === 2}
+                >
                     <Image 
                         src={galleryPic2Img}
                         alt="A room"
     
                     />
                 </div>
-                <div className={styles.Image + ' ' + styles.Image_3}>
+                <div className={styles.Image + ' ' + styles.Image_3}
+                    onClick={() => setCurrentImage(3)}
+                    onMouseLeave={() => setCurrentImage(0)}
+                    data-expanded={currentImage === 3}
+                >
                     <Image 
                         src={galleryPic3Img}
                         alt="A room"
