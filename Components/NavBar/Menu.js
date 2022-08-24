17e63fa6411c8ad15039bf5ab3b67616f0d75a6e
Minh Le <duyminh.le310@gmail.com>
2022-08-23 23:29:32 -0400

    Menu Expanded

--- Components/NavBar/Menu.js
+++ Components/NavBar/Menu.js
@@ -6,11 +6,42 @@ export default function Menu() {
     return (
         <div className={styles.menu}>
             <ul className={styles.ul}>
-                <li><button href="/property">PROPERTY</button></li>
-                <li><Link href="/services">SERVICES</Link></li>
-                <li><Link href="/testimonial">TESTIMONIAL</Link></li>
-                <li><Link href="/about">ABOUT</Link></li>
+                <li>
+                    <button href="/property">PROPERTY</button>
+                    {/* <div className={styles.menuExpanded}>
+                        <Property />
+                    </div>     */}
+
+                </li>
+                <li>
+                    <Link href="/services">SERVICES</Link>
+
+
+                </li>
+                <li>
+                    <Link href="/testimonial">TESTIMONIAL</Link>
+
+                </li>
+                <li>
+                    <Link href="/about">ABOUT</Link>
+
+                </li>
             </ul>
         </div>
     )
 }
+
+const Property = () => (
+    <>
+        <div>
+            <h5>Category 1</h5>
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+                <li>Link 4</li>
+                <li>Link 5</li>
+            </ul>
+        </div>
+    </>
+)
