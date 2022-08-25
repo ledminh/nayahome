29ede96e37fee9b3e6530160cb14ab677f2b4b79
Minh Le <duyminh.le310@gmail.com>
2022-08-24 22:26:44 -0400

    Add all menuExpanded

--- Components/NavBar/Menu.js
+++ Components/NavBar/Menu.js
@@ -6,33 +6,48 @@ import Property from './Property';
 import Services from './Services';
 import About from './About';
 
+const menuItems = [
+    {
+        name: 'PROPERTY',
+        expandedMenu: Property
+    },
+
+    {
+        name: 'SERVICES',
+        expandedMenu: Services
+    },
+
+    {
+        name: 'TESTIMONIAL',
+        link: '/testimonial'
+    },
+
+    {
+        name: 'ABOUT',
+        expandedMenu: About
+    }
+]
+
 export default function Menu() {
     return (
         <div className={styles.menu}>
             <ul className={styles.ul}>
-                <li className={styles.li}>
-                    <button href="/property">PROPERTY</button>
-                    <div className={styles.menuExpanded}>
-                    </div>    
-
-                </li>
-                <li className={styles.li}>
-                    <Link href="/services">SERVICES</Link>
-                    <div className={styles.menuExpanded}>
-                    </div>
-
-                </li>
-                <li className={styles.li}>
-                    <Link href="/testimonial">TESTIMONIAL</Link>
-
-                </li>
-                <li className={styles.li}>
-                    <Link href="/about">ABOUT</Link>
-                    <div className={styles.menuExpanded}>
-                        <About />
-                    </div>
-
-                </li>
+                {
+                    menuItems.map((item, i) => (
+                        <li className={styles.li} key = {item.name}>
+                            {
+                                item.expandedMenu? 
+                                    (   <>
+                                            <button>{item.name}</button>
+                                            <div className={styles.menuExpanded + (i < (menuItems.length - 1)/2? ' ' + styles.left : ' ' + styles.right)}>
+                                                <item.expandedMenu/>
+                                            </div>
+                                        </>):
+                                    (<Link href={item.link}>{item.name}</Link>) 
+                            }                  
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
