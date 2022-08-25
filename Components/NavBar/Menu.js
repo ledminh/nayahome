517c3903a7692a4d065c2a48e61024fdb451ec9a
Minh Le <duyminh.le310@gmail.com>
2022-08-24 23:22:31 -0400

    Finish Menu

--- Components/NavBar/Menu.js
+++ Components/NavBar/Menu.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Link from 'next/link';
 
 import styles from './Menu.module.scss';
@@ -29,17 +31,44 @@ const menuItems = [
 ]
 
 export default function Menu() {
+    const [currDataExpanded, setCurrDataExpanded] = useState(0);
+
+    useEffect(() => {
+        const escape = () => setCurrDataExpanded(0);
+
+        window.document.addEventListener('click', escape);
+
+        return () => window.document.removeEventListener('click', escape);
+    }, []);
+
     return (
         <div className={styles.menu}>
             <ul className={styles.ul}>
                 {
                     menuItems.map((item, i) => (
-                        <li className={styles.li} key = {item.name}>
+                        <li className={styles.li} key = {item.name}
+                            data-expanded={currDataExpanded === i + 1}
+                        >
                             {
                                 item.expandedMenu? 
                                     (   <>
-                                            <button>{item.name}</button>
-                                            <div className={styles.menuExpanded + (i < (menuItems.length - 1)/2? ' ' + styles.left : ' ' + styles.right)}>
+                                            <button
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+
+                                                    if(currDataExpanded === i + 1){
+                                                        setCurrDataExpanded(0);
+                                                    }
+                                                    else
+                                                        setCurrDataExpanded(i + 1)
+                                                
+                                                }}
+                                                >{item.name}</button>
+                                            <div 
+                                                className={styles.menuExpanded + (i < (menuItems.length - 1)/2? ' ' + styles.left : ' ' + styles.right)}
+                                                data-expanded={currDataExpanded === i + 1}
+                                                onClick={(e) => e.stopPropagation()}
+                                                >
                                                 <item.expandedMenu/>
                                             </div>
                                         </>):
