e7f69443ef16283fb5ca90f77a5a7c57a9ad9dbc
Minh Le <duyminh.le310@gmail.com>
2022-08-25 16:22:18 -0400

    Add chatbox on LetsTalk

--- Components/NavBar/LetsTalk.js
+++ Components/NavBar/LetsTalk.js
@@ -5,6 +5,12 @@ export default function LetsTalk() {
     return (
         <div className={styles.LetsTalk}>
             <button className={styles.button}>LET{"'S"} TALK</button>
+            <div className={styles.chatbox}>
+                <div className={styles.chatScreen}>
+                    <p>Hello, how can I help you today?</p>
+                </div>
+                <input />
+            </div>
         </div>
 
     )
