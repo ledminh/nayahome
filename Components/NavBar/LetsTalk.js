d3529714cdddb252410ebb89e7b70c321a935830
Minh Le <duyminh.le310@gmail.com>
2022-08-26 16:28:47 -0400

    Finish

--- Components/NavBar/LetsTalk.js
+++ Components/NavBar/LetsTalk.js
@@ -1,15 +1,54 @@
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './LetsTalk.module.scss';
+
+import xSymbolImg from '../../imgs/x-symbol.png';
+
+
+import Image from 'next/image';
 
 export default function LetsTalk() {
     const [chatStream, setChatStream] = useState([]);
+    const [opened, setOpened] = useState(false);
 
+    useEffect(() => {
+        const clickHandle = (e) => {
+            e.preventDefault();
+            setOpened(false);
+        }
+
+        window.document.addEventListener('click', clickHandle);
+
+        return () => window.document.removeEventListener('click', clickHandle);
+
+    }, []);
     return (
-        <div className={styles.LetsTalk}>
-            <button className={styles.button}>LET{"'S"} TALK</button>
-            <div className={styles.chatbox}>
-                <h5 className={styles.title}>LET{"'"}S TALK</h5>
+        <div className={styles.LetsTalk}
+                onClick={(e) => e.stopPropagation()}
+            >
+            <button className={styles.button}
+                data-opened={opened}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setOpened(true);
+                }}
+            >
+                LET{"'S"} TALK
+            </button>
+            <div className={styles.chatbox}
+                data-opened={opened}
+            >
+                <h5 className={styles.title}>
+                    <span>LET{"'"}S TALK</span>
+                    <button className={styles.close}
+                        onClick={() => setOpened(false)}
+                    >
+                        <Image 
+                            src={xSymbolImg}
+                            alt="X mark"
+                        />
+                    </button>
+                </h5>
                 <ChatScreen chatStream={chatStream}/>               
                 <Input onSubmit={(val) => setChatStream([...chatStream, val])}/>    
             </div>
